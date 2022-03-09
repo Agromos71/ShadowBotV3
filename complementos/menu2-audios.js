e3c29b292d7764170592bb72f3a36563a2032965
Agromos71 <101158427+Agromos71@users.noreply.github.com>
2022-03-09 11:10:23 -0600

    Update menu2-audios.js

--- complementos/menu2-audios.js
+++ complementos/menu2-audios.js
@@ -12,7 +12,7 @@ let { name, limit, exp, banned, lastclaim, registered, regTime, age, level } = g
 let { min, xp, max } = levelling.xpRange(level, global.multiplier)
 let username = conn.getName(who)
 let str = `
-*_ミ💖 Hola ${username} 💖彡_*
+*_ミ💚 Hola ${username} 💚彡_*
 
 *<MENU DE AUDIOS/>*
 - Escribe las palabras/frases tal como estan, no hace falta poner ningun prefijo (#, ., *, etc) 

6cb7e9efd0f0276644c935c06b287b30489afad4
Agromos71 <101158427+Agromos71@users.noreply.github.com>
2022-03-08 23:10:33 -0600

    Update and rename plugins/instalarbot.js to complementos/instalarbot.js

--- complementos/instalarbot.js
+++ complementos/instalarbot.js
@@ -11,7 +11,7 @@ let handler = async m => m.reply(`
 ➤ 2 teléfonos o un 1 teléfono y una PC (es temporal - solo para escanear un código QR)
 
 💥 𝐑𝐄𝐏𝐎𝐒𝐈𝐓𝐎𝐑𝐈𝐎 𝐃𝐄𝐋 𝐁𝐎𝐓 💥
-➤ https://github.com/BrunoSobrino/ShadowBotV3
+➤ https://github.com/Agromos71/ShadowBotV3
 
 💥 𝐀𝐑𝐄𝐀 𝐃𝐄 𝐃𝐄𝐒𝐂𝐀𝐑𝐆𝐀𝐒 💥
 ➤ Termux: https://f-droid.org/en/packages/com.termux/
@@ -23,7 +23,7 @@ let handler = async m => m.reply(`
 ➤ apt install nodejs -y
 ➤ apt install ffmpeg -y
 ➤ apt install imagemagick -y
-➤ git clone https://github.com/BrunoSobrino/ShadowBotV3
+➤ git clone https://github.com/Agromos71/ShadowBotV3
 ➤ cd ShadowBotV3
 ➤ npm install
 ➤ npm install -g npm@8.4.0
@@ -36,7 +36,7 @@ let handler = async m => m.reply(`
 ➤ Para habilitar el /añadir y el /sacar usa #enable autoread
 - Puede ocasionar que el numero se vaya a soporte 
 ➤ PayPal para apoyo voluntario: https://www.paypal.me/TheShadowBrokers133
-➤ Numero de ayuda durante la instalacion: wa.me/5219992095479 (No Bot, solo ayuda durante la instalación)
+➤ Numero de ayuda durante la instalacion: wa.me/16178196212(No Bot, solo ayuda durante la instalación)
 
 *◄┢┅͜͡✇⟬↯ື ►ஜ۩💥۩ஜ◄ ↯ື⟭✇͜͡┅┧►*
 `.trim()) 

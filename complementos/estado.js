426f590a3fc4a5edf561210a4f60625296b5e7f9
Agromos71 <101158427+Agromos71@users.noreply.github.com>
2022-03-09 11:05:44 -0600

    Update estado.js

--- complementos/estado.js
+++ complementos/estado.js
@@ -2,11 +2,11 @@ let handler = async (m, { conn, command, usedPrefix }) => {
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let mentionedJid = [who]
 conn.send3Button(m.chat, `
-*ミ💖 Hola estimado usuario 💖彡*
+*ミ👻 Hola estimado usuario 👻彡*
 
-*ミ➡ Estado del Bot⬅️彡*
+*ミ➡ Estado del Bot✅彡*
 *=> Bot activo ✔️*
 *=> Bot uso público ✔️*
-`.trim(), '©AGROMOS  SP Bot', '🛡 MENÚ 🛡', `${usedPrefix}menu`, '🔰 MENÚ SIMPLE 🔰', `${usedPrefix}menusimple`, '🔱 MENÚ AUDIOS 🔱', `${usedPrefix}menuaudios`, m, false, { contextInfo: { mentionedJid }})}
+`.trim(), '©AGROMOS  SP Bot', '⚠️ MENÚ ⚠️', `${usedPrefix}menu`, '⚜MENÚ SIMPLE⚜', `${usedPrefix}menusimple`, '⏩MENÚ AUDIOS⏮', `${usedPrefix}menuaudios`, m, false, { contextInfo: { mentionedJid }})}
 handler.command = /^(estado|status|estate|state|stado|stats)$/i
 module.exports = handler

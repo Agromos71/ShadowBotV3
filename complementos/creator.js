d0e2b149e73900844d06a5e66e99b9249b450733
Agromos71 <101158427+Agromos71@users.noreply.github.com>
2022-03-09 12:36:07 -0600

    Delete complementos directory

--- complementos/creator.js
+++
@@ -1,5 +0,0 @@
-function handler(m) {
-this.sendContact(m.chat, global.agromos[0], this.getName(global.owner[0] + '@s.whatsapp.net'), m)
-this.sendContact(m.chat, '16178196212, 'Agromos - Creador - Oficial', m)}
-handler.command = /^(contacto|agromos sp|creator|creador|propietario|dueño)$/i
-module.exports = handler

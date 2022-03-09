f10a0ede4352e97a399333f2ec12bc9253e1fac6
Agromos71 <101158427+Agromos71@users.noreply.github.com>
2022-03-09 12:54:00 -0600

    Create estado.js

---
+++ plugins/estado.js
@@ -0,0 +1,12 @@
+let handler = async (m, { conn, command, usedPrefix }) => {
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let mentionedJid = [who]
+conn.send3Button(m.chat, `
+*ミ👻 Hola estimado usuario 👻彡*
+
+*ミ➡ Estado del Bot✅彡*
+*=> Bot activo ✔️*
+*=> Bot uso público ✔️*
+`.trim(), '©AGROMOS  SP Bot', '⚠️ MENÚ ⚠️', `${usedPrefix}menu`, '⚜MENÚ SIMPLE⚜', `${usedPrefix}menusimple`, '⏩MENÚ AUDIOS⏮', `${usedPrefix}menuaudios`, m, false, { contextInfo: { mentionedJid }})}
+handler.command = /^(estado|status|estate|state|stado|stats)$/i
+module.exports = handler

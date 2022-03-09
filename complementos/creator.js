d0b1ab523ba17cd1c7a0290722a47594e6c22467
Agromos71 <101158427+Agromos71@users.noreply.github.com>
2022-03-08 20:49:19 -0600

    Update and rename plugins/creator.js to complementos/creator.js

---
+++ complementos/creator.js
@@ -0,0 +1,5 @@
+function handler(m) {
+this.sendContact(m.chat, global.agromos[0], this.getName(global.owner[0] + '@s.whatsapp.net'), m)
+this.sendContact(m.chat, '16178196212, 'Shadow - Creador - Oficial', m)}
+handler.command = /^(contacto|agromos sp|creator|creador|propietario|dueño)$/i
+module.exports = handler

5dd135e1b2837095df40b5c5f8fc80a479f8c132
Agromos71 <101158427+Agromos71@users.noreply.github.com>
2022-03-09 11:13:30 -0600

    Update grupos.js

--- complementos/grupos.js
+++ complementos/grupos.js
@@ -2,7 +2,7 @@ let handler  = async (m, { conn, usedPrefix: _p }) => {
 let info = `
 *Hola 👋🏻, unete a los grupos oficiales para pasar un rato agradable usando el Bot o platicando con la familia de The Shadow Borkers - Bot*
 
-*➤ Grupos oficiales del Bot:*
+*➤ Grupos oficial del Bot:*
 *1.- https://chat.whatsapp.com/H1UqcwM5TeZFjt7LELOVNS*
 
 `.trim() 

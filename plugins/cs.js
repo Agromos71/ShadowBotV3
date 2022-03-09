52925f01bd7c24ebe79ef9190315654c89dc7403
Agromos71 <101158427+Agromos71@users.noreply.github.com>
2022-03-09 13:24:31 -0600

    Create cs.js

---
+++ plugins/cs.js
@@ -0,0 +1,16 @@
+//Codigo by ShadowBotV3
+let fetch = require('node-fetch')
+let handler = async (m, { conn, usedPrefix }) => {
+let who = m.sender
+let pp = await(await fetch('https://i.imgur.com/oHrJTtq.jpg')).buffer()
+let kai = await(await fetch('https://i.imgur.com/oHrJTtq.jpg')).buffer()
+let text = `*https://github.com/Agromos71/ShadowBotV3*`.trim()
+conn.reply(m.chat, text, m, { thumbnail: kai, contextInfo: {
+mentionedJid: [who],
+externalAdReply: {
+title: '𝚁𝙴𝙿𝙾𝚂𝙸𝚃𝙾𝚁𝙸𝙾 𝙳𝙴𝙻 𝙱𝙾𝚃',
+body: '☣AGROMOS SP BOT☣',
+thumbnail: pp
+}}})}
+handler.command = /^sc|script|repositorio|repobot|scbot$/i
+module.exports = handler
